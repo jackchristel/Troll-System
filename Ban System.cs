cba8814e89e12353751b9eba946993416b5e6430
Jack Christel <mulcher19@gmail.com>
2020-08-22 22:38:33 -0400

    Changing File names

---
+++ Ban System.cs
@@ -0,0 +1,16 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("Ban Tool", "Grave", "0.1.0")]
+    [Description("A simple admin tool to ban people by using a command in-game.")]
+    class BanSystem : CovalencePlugin
+    {
+        private void Init()
+        {
+            Puts("A baby plugin is born!");
+        }
+
+        // The rest of the code magic
+
+        // TODO (you): Make more epic stuff
+    }
+}
