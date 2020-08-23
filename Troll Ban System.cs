0fdf08cce977152513acf71a630c654891dd386c
Jack Christel <mulcher19@gmail.com>
2020-08-23 00:28:19 -0400

    commit

---
+++ Troll Ban System.cs
@@ -0,0 +1,16 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("Troll Ban System", "Grave", "0.1.0")]
+    [Description("A easier way to troll ban someone in rust! doing /ban announces a server message where it says that a player was banned, and /unban announces that a player was unbanned all without actually banning or unbanning anyone!")]
+    class TrollBanSystem : CovalencePlugin
+    {
+        private void Init()
+        {
+            Puts("A baby plugin is born!");
+        }
+
+        // The rest of the code magic
+
+        // TODO (you): Make more epic stuff
+    }
+}
