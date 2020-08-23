560f9f6013cb3107e4a8b5e5d224604eb17c7fee
Jack Christel <mulcher19@gmail.com>
2020-08-23 18:47:34 -0400

    changes

--- Troll Ban System.cs
+++ Troll Ban System.cs
@@ -1,7 +1,12 @@
-﻿namespace Oxide.Plugins
+﻿using ConVar;
+using Network;
+using Oxide.Core.Libraries.Covalence;
+using Oxide.Game.Rust.Libraries;
+
+namespace Oxide.Plugins
 {
     [Info("Troll Ban System", "Grave", "0.1.0")]
-    [Description("A easier way to troll ban someone in rust! doing /ban announces a server message where it says that a player was banned, and /unban announces that a player was unbanned all without actually banning or unbanning anyone!")]
+    [Description("A easier way to troll ban someone in rust! doing /tban announces a server message where it says that a player was banned, and /tunban announces that a player was unbanned all without actually banning or unbanning anyone!")]
     class TrollBanSystem : CovalencePlugin
     {
         private void Init()
@@ -9,8 +14,11 @@
             Puts("A baby plugin is born!");
         }
 
-        // The rest of the code magic
-
-        // TODO (you): Make more epic stuff
+        [Command("tban")]
+        private void BanCommand(IPlayer player, string command, string[] args)
+        {
+            Message = string.Join(" ", args);
+        }
+        
     }
 }
