4a693aa6236388bedafd43a171b1686564901555
Jack Christel <mulcher19@gmail.com>
2020-08-24 12:36:19 -0400

    Added Language API Usage and fixed Permissions

--- TrollBanSystem.cs
+++ TrollBanSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Oxide.Core.Libraries.Covalence;
+using System.Collections.Generic;
 
 namespace Oxide.Plugins
 {
@@ -8,21 +9,37 @@ namespace Oxide.Plugins
     {
         private void Init()
         {
-            Puts("A baby plugin is born!");
-            permission.RegisterPermission("tban.use", this);
-            permission.RegisterPermission("tunban.use", this);
+            permission.RegisterPermission("trollbansystem.ban", this);
+            permission.RegisterPermission("trollbansystem.unban", this);
         }
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["BanMessage"] = "{Banner} banned {Bannedname}",
+                ["UnBanMessage"] = "{UnBanner} unbanned {Bannedname}"
+            }, this);
+        }
+       
 
-        [Command("tban"),Permission("tban.use")]
+        [Command("tban"),Permission("trollbansystem.ban")]
         private void Cmdtban(IPlayer player, string command, string[] args)
         {
             server.Broadcast($"{player.Name} banned {string.Join(" ", args)}");
+            string message = lang.GetMessage("BanMessage", this, player.Id)
+     .Replace("{Banner}", player.Name)
+     .Replace("{Bannedname}", string.Join(" ", args));
+            Puts(message);
         }
 
-        [Command("tunban"),Permission("tunban.use")]
+        [Command("tunban"),Permission("trollbansystem.unban")]
         private void Cmdtunban(IPlayer player, string command, string[] args)
         {
             server.Broadcast($"{player.Name} unbanned {string.Join(" ", args)}");
+            string message = lang.GetMessage("UnBanMessage", this, player.Id)
+     .Replace("{UnBanner}", player.Name)
+     .Replace("{Bannedname}", string.Join(" ", args));
+            Puts(message);
         }
 
     }
