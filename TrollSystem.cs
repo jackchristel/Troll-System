14aa5f3c1a7bb4bb8bc22d53b9af461113071bcd
Jack Christel <mulcher19@gmail.com>
2021-08-05 22:45:15 -0400

    Added Muting/Unmuting Command

---
+++ TrollSystem.cs
@@ -0,0 +1,71 @@
+﻿using Oxide.Core.Libraries.Covalence;
+using System.Collections.Generic;
+
+namespace Oxide.Plugins
+{
+    [Info("Troll System", "Grave", "0.6.0")]
+    [Description("A easier way to troll someone in Rust! The commands available are banning and muting, /tmute, /tunmute, /tban, /tunban, and they all annouce in the chat. Example: /tmute Grave - Announces Grave muted Grave, as I sent the command. This works with unmuting and banning/unbanning.")]
+    class TrollSystem : CovalencePlugin
+    {
+        private void Init()
+        {
+            permission.RegisterPermission("trollsystem.ban", this);
+            permission.RegisterPermission("trollsystem.unban", this);
+            permission.RegisterPermission("trollsystem.mute", this);
+            permission.RegisterPermission("trollsystem.unmute", this);
+        }
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["BanMessage"] = "{Banner} banned {Bannedname}",
+                ["UnBanMessage"] = "{UnBanner} unbanned {Bannedname}",
+                ["MuteMessage"] = "{Muter} muted {Mutedname}",
+                ["UnMuteMessage"] = "{UnMuter} unmuted {Mutedname}"
+            }, this);
+        }
+       
+
+        [Command("tban"),Permission("trollsystem.ban")]
+        private void Cmdtban(IPlayer player, string command, string[] args)
+        {
+            server.Broadcast($"{player.Name} banned {string.Join(" ", args)}");
+            string message = lang.GetMessage("BanMessage", this, player.Id)
+     .Replace("{Banner}", player.Name)
+     .Replace("{Bannedname}", string.Join(" ", args));
+            Puts(message);
+        }
+
+        [Command("tunban"),Permission("trollsystem.unban")]
+        private void Cmdtunban(IPlayer player, string command, string[] args)
+        {
+            server.Broadcast($"{player.Name} unbanned {string.Join(" ", args)}");
+            string message = lang.GetMessage("UnBanMessage", this, player.Id)
+     .Replace("{UnBanner}", player.Name)
+     .Replace("{Bannedname}", string.Join(" ", args));
+            Puts(message);
+        }
+
+        [Command("tmute"),Permission("trollsystem.mute")]
+        private void Cmdtmute(IPlayer player, string command, string[] args)
+        {
+            server.Broadcast($"{player.Name} muted {string.Join(" ", args)}");
+            string message = lang.GetMessage("MuteMessage", this, player.Id)
+                .Replace("{Muter}", player.Name)
+                .Replace("{Mutedname}", string.Join(" ", args));
+            Puts(message);
+
+        }
+        [Command("tunmute"), Permission("trollsystem.unmute")]
+        private void Cmdtunmute(IPlayer player, string command, string[] args)
+        {
+            server.Broadcast($"{player.Name} unmuted {string.Join(" ", args)}");
+            string message = lang.GetMessage("UnMuteMessage", this, player.Id)
+                .Replace("{UnMuter}", player.Name)
+                .Replace("{Mutedname}", string.Join(" ", args));
+            Puts(message);
+
+        }
+
+    }
+}
